9859a7e9c2f54634676fd863252edc54fee3a17f
wangdong <wangdong@zhongan.io>
2021-02-06 20:56:02 +0800

    more

--- src/index.ts
+++ src/index.ts
@@ -43,11 +43,7 @@ const main = async () => {
 
       if (event.topics[0] === eventBlockSubmitted) {
         const data = await extractBlock(web3, event)
-        await writeJsonFile(
-          './blocks/',
-          data.block.blockNumber + '-' + data.block._id,
-          data
-        )
+        await writeJsonFile('./blocks/', 'block_' + data.block._id, data)
         await persister.persistBlock(data)
         console.log(
           'block:',
@@ -62,11 +58,7 @@ const main = async () => {
       } else {
         const token = await extractToken(web3, event)
         if (token) {
-          await writeJsonFile(
-            './tokens/',
-            token.blockNumber + '-' + token._id,
-            token
-          )
+          await writeJsonFile('./tokens/', 'token_' + token._id, token)
           await persister.persistToken(token)
           console.log(
             'token:',
